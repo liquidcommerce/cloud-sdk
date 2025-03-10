c553d26c1a87d4c6a0ee944dd2f261a674f3f8f5
Elio Mehmeti <81304859+Elio1995@users.noreply.github.com>
2025-03-10 20:56:04 +0100

    Update address.interface.ts

--- src/interfaces/address.interface.ts
+++ src/interfaces/address.interface.ts
@@ -114,7 +114,7 @@ export interface IAddressDetailsParams extends ICoreParams {
  *
  * @property {string} formattedAddress - The formatted address as a string.
  * @property {ICoords} coords - The coordinates associated with the address.
- * @property {Omit<IAddress, 'id'>} addressComponents - The address components.
+ * @property {Omit<IAddress, 'id'>} address - The address object containing all address fields except the id property.
  */
 export interface IAddressDetailsResult {
   formattedAddress: string;
