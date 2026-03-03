22c0377e9162d0fadf91fdacbf5dbdc75e9b894e
Mateo Kadiu <mateokadiu17@gmail.com>
2026-03-03 17:50:54 +0100

    fix(constants): handle undefined environment variables for base URLs

--- src/constants/core.constant.ts
+++ src/constants/core.constant.ts
@@ -8,9 +8,9 @@ import { LIQUID_COMMERCE_ENV } from '../enums';
  * The values are URLs read from the respective environment variables: `ENV_LOC`, `ENV_DEV`, `ENV_STAGE`, and `ENV_PROD`.
  * If the environment variables are not set, the values default to an empty string.
  */
-export const DEFAULT_BASE_URLS: Record<string, string> = {
-  [LIQUID_COMMERCE_ENV.LOC]: process.env.ENV_LOC as string,
-  [LIQUID_COMMERCE_ENV.DEV]: process.env.ENV_DEV as string,
-  [LIQUID_COMMERCE_ENV.STAGE]: process.env.ENV_STAGE as string,
-  [LIQUID_COMMERCE_ENV.PROD]: process.env.ENV_PROD as string,
+export const DEFAULT_BASE_URLS = {
+  [LIQUID_COMMERCE_ENV.LOC]: process.env.ENV_LOC,
+  [LIQUID_COMMERCE_ENV.DEV]: process.env.ENV_DEV,
+  [LIQUID_COMMERCE_ENV.STAGE]: process.env.ENV_STAGE,
+  [LIQUID_COMMERCE_ENV.PROD]: process.env.ENV_PROD,
 };
